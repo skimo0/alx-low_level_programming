12467e29f8e03dc5033edab4f46fab608190ccf6
skimo0 <mogashoop@outlook.com>
2023-08-11 09:19:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
 */
 
 int main(void)
-{char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+{
+	char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
