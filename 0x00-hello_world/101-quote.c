26c292596174ac2d03de85e955f2e0e4411a09bf
skimo0 <mogashoop@outlook.com>
2023-08-11 09:08:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  * Description: print a quote write function
- *    ssize_t wrint(int fd, cont void *buf, size_t count);
+ *    ssize_t wrint(int fd, const void *buf, size_t count);
  *
  * Return: 1 (error).
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+{char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 
 	write(1, quo, 59)
 	return (1);
