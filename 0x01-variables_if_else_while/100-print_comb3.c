2535ca632b90197d30c3d6489243afc4ff357705
skimo0 <mogashoop@outlook.com>
2023-08-12 05:21:37 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.md>
+
+/**
+ * main - entry point
+ *
+ * Description:
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 9)
+	{
+		if digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48)
+				puchtar(digit2 + 48)
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',')
+					putchar(' ')
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
