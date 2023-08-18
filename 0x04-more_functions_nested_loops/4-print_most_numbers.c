0a60d79b2bf0372325cb6a8c83b6ebd4847fd283
skimo0 <mogashoop@outlook.com>
2023-08-18 07:40:38 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,8 @@
-#include "main.c"
+#include "main.h"
 
 /**
- * print_most_numbers - Prints 0 - 9 apart
- * from 2 and 4 and you can
- * only use _putchar twice
+ * print_most_numbers - Outputs digits 0 to 9, excluding 2 and 4,
+ * using the _putchar function only twice.
  * Return: Always 0 (success)
  */
 
