340d18fcb65ee0cb3e870663b2a5576d741d4481
skimo0 <mogashoop@outlook.com>
2023-08-18 09:10:25 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonal line using backslashes.
+ * @n: Number of backslashes in the diagonal line
+*/
+
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
