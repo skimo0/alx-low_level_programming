c052ae31132b77865e542adb7fb990d8446cf150
skimo0 <mogashoop@outlook.com>
2023-08-24 07:20:33 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that concatenates two strings
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most muster of bytes from @src
+ *
+ * Return: @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int s, i;
+
+	s = 0;
+	while (dest[s])
+		s++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[s + i] = src[i];
+
+	dest[s + i] = '\0';
+
+	return (dest);
+}
