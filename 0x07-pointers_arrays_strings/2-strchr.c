76ee7a5cad992abdd2f9447f4232c1de388713d5
skimo0 <mogashoop@outlook.com>
2023-08-29 04:12:25 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,11 +2,11 @@
 
 
 /**
- * _strchr - Locates the first occurrence of a character in a string
- * @s: Pointer to the string to search in
- * @c: Character to search for
+ * _strchr - locates the first occurrence of a character in a string
+ * @s: pointer to the string to search in
+ * @c: character to search for
  *
- * Return: Pointer to the first occurrence of the character c in the string s,
+ * Return: pointer to the first occurrence of the character c in the string s,
  *         or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
