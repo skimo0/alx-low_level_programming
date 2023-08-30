b4ec46fef020d4b484a9d38a184579a0828dab69
skimo0 <mogashoop@outlook.com>
2023-08-30 13:12:37 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - returns the length of a string
+ * @s: Pointer to the string to be printed.
+ * Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+	{
+		n += _strlen_recursion(s + 1) + 1;
+	}
+	return (n);
+}
