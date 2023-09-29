cec8ce96eca846df1d345ae481bc759878612541
skimo0 <mogashoop@outlook.com>
2023-09-29 12:26:13 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,11 +23,11 @@ int is_palindrome(char *s)
  *
  * Return: length of the string
 */
-int _string_length(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _string_length(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
