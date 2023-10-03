e0d3ab4d28f10b40836e408d61daf895428143ec
skimo0 <mogashoop@outlook.com>
2023-10-03 02:22:26 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -5,9 +5,10 @@
  * _putchar - Writes the character 'c' to stdout.
  * @c: The character to print.
  *
- * Returns: On success, returns 1. On error, returns -1 and sets errno appropriately.
+ * Return: On success 1
+ * On error, -1 is returned and errno is set appropriately.
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
