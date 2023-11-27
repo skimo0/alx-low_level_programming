1ade56b89d121baeceb65af714529690ff13628f
skimo0 <Mogashoop@outlook.com>
2023-11-27 15:30:05 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: doh's name
+ * @age: dog's age
+ * @owner: ower's name
+ *
+ * Description: struct dog with the following elements
+ */
+
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};      
+
+#endif
