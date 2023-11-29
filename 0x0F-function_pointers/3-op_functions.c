40508617e4797f53152d017a1ab13502dd68a118
skimo0 <Mogashoop@outlook.com>
2023-11-29 13:04:52 -0800

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,47 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: tghe product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
